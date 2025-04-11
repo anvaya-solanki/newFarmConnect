5f1ecc14888c5810830d28451d35d7585d2459ee
anvaya-solanki <anvayasolanki@gmail.com>
2025-04-11 13:13:31 +0530

    Admin auth done

--- client/src/components/account/FormHeading.jsx
+++ client/src/components/account/FormHeading.jsx
@@ -1,24 +1,6 @@
-// import React from "react";
-
-// const FormHeading = ({ type, isSignInForm }) => {
-//   return (
-//     <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-//       {isSignInForm?"Sign In to":"Create"}{" "}
-//       <span
-//         className={`${type === "seller" ? "text-green-700" : "text-blue-600"}`}
-//       >
-//         {type === "user" ? "User" : "Seller"}
-//       </span>{" "}
-//       account
-//     </h2>
-//   );
-// };
-
-// export default FormHeading;
 import React from "react";
 
 const FormHeading = ({ type, isSignInForm }) => {
-  // Choose the display label and color based on account type
   const getAccountLabel = (type) => {
     if (type === "admin") return "Admin";
     if (type === "seller") return "Seller";
